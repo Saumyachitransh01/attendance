53a397c515b4f4b724f532a0e3f2e4124635a5cf
ansh-bajaj <a.anshb.bajaj@gmail.com>
2019-09-11 16:40:23 +0530

    first

--- Attendance2.py
+++ Attendance2.py
@@ -4,33 +4,49 @@ import csv
 
 
 def addParticipant():
-    f = open('Registrations.csv', "a")
-    print("Enter Name:")
-    name = str(input())
-    print("Enter Branch:")
-    branch = str(input())
-    print("Enter Year:  (1,2,3 or 4)")
-    year = int(input())
-    print("For or Against:")
-    side = str(input())
-    print("Enter Phone Number:")
-    phone = input()
-    print("Enter Email Address:")
-    email = str(input())
-    row = [name, branch, year, side, phone, email, '1']
-    file1 = csv.writer(f)
-    file1.writerow(row)
-    print("Record Added!")
+    f = open('On the Spot.csv', "a")
+    more='y'
+    while more=='y':
+        print("Enter Name:")
+        name = str(input())
+        print("Enter Branch:")
+        branch = str(input())
+        print("Enter Year:  (1,2,3 or 4)")
+        year = int(input())
+        print("For or Against:")
+        side = str(input())
+        print("Enter Phone Number:")
+        phone = input()
+        print("Enter Email Address:")
+        email = str(input())
+        row = [name, branch, year, side, phone, email, '1']
+        file1 = csv.writer(f)
+        file1.writerow(row)
+        print("Record Added!")
+        more=input("Add more?")
     f.close()
 
-
 def attendance():
-    df = pd.read_csv('Registrations.csv')
-    df.dropna(inplace=True)
-    print("Enter Name:")
-    name = str(input())
-    df['Name'].str.find(name)
-
+    more='y'
+    flag=1
+    while more=='y':
+        df = pd.read_csv('Registrations.csv')
+        df2 = pd.read_csv('Final Attendance.csv')
+        attendees1=[]
+        df.dropna(inplace=True)
+        print("Enter Name:")
+        name = str(input())
+        df2= df[df["Name"].str.contains(name)]
+        print(df2)
+        if(flag==1):
+            export_csv=df2.to_csv ('Final Attendance.csv', index = None, header=True)
+            flag=0
+        else:
+            attendees1=df2.values.tolist()
+            f2=open('Final Attendance.csv',"a")
+            file2=csv.writer(f2)
+            file2.writerow(attendees1)
+        more=input("Mark Attendance of more Students?")
 
 def extract():
     df = pd.read_csv('Registrations.csv')
@@ -51,6 +67,8 @@ if __name__ == '__main__':
             attendance()
         elif p == 3:
             extract()
+        else:
+            print("Invalid Input. ")
         print("Press y to continue")
         ch = input()
         if not (ch == 'y' or ch == 'Y'):
